f0148ca1a7a193356f0b3bc0e180b445513be6e6
taivoit1993 <taivo.mile@gmail.com>
2023-09-24 23:55:03 +0700

    golang variable example

---
+++ Golang-Fundamental/Variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int
+	var name string
+	var isStudent bool
+	var temperature float64
+
+	age = 30
+	name = "John Doe"
+	isStudent = true
+	temperature = 28.90
+
+	fmt.Println("Age:", age)
+	fmt.Println("Name:", name)
+	fmt.Println("Student:", isStudent)
+	fmt.Println("Temperature:", temperature)
+}
